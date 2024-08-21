778358845018e0170f12d783421a37f49f65c1b1
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-21 20:33:20 +0200

    feat: Update home page layout for centered alignment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-center text-2xl mb-10">Street Fighter Combo list</h1>
-      <ul className="flex">
+      <ul className="flex justify-center">
         <li>
           <a
             className="bg-[url('/juri.png')] w-[575px] h-[625px] block hover:bg-[url('/juri_over.png')]"
