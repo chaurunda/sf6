877757845acca5f08bd29e49ddeafc088e65fab4
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-25 12:10:22 +0200

    fix: change the way I load background image

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Svgloader from "./components/Svgloader"
 
 export default function Home() {
   const characters = ["juri", "cammy"]
+  const background: { [key: string]: string } = {
+    juri: "bg-[url('/juri.png')] hover:bg-[url('/juri_over.png')]",
+    cammy: "bg-[url('/cammy.png')] hover:bg-[url('/cammy_over.png')]",
+  }
   return (
     <main>
       <h1 className="text-center text-2xl mb-10">Street Fighter Combo list</h1>
@@ -10,7 +14,7 @@ export default function Home() {
         {characters.map((character) => (
           <li key={character}>
             <a
-              className={`bg-[url('/${character}.png')] w-[575px] h-[625px] block hover:bg-[url('/${character}_over.png')]`}
+              className={`${background[character]} w-[575px] h-[625px] block`}
               href={`/${character}`}
             ></a>
           </li>
