9c73ffce3c953a42def3f6fb14b450f9b62a3491
Huc Arnaud <arnaud.huc@adevinta.com>
2025-03-10 17:56:42 +0100

    feat: improve styling of Notation component for better readability

--- app/components/Notation/index.tsx
+++ app/components/Notation/index.tsx
@@ -9,17 +9,17 @@ const Notation = ({}: NotationProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="my-10">
+    <div className="my-10 bg-slate-800 p-2 rounded-lg">
       <h2>
         <button onClick={() => setIsOpen(!isOpen)}>Notation {isOpen ? "↑" : "↓"}</button>
       </h2>
       <table className={`table-fixed mt-3 ${isOpen ? "" : "hidden"}`}>
         <thead>
           <tr>
-            <th className="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">
+            <th className="border-b border-slate-600 font-medium p-2 pl-8 pt-0 pb-3 text-slate-200 text-left">
               Name
             </th>
-            <th className="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">
+            <th className="border-b border-slate-600 font-medium p-2 pl-8 pt-0 pb-3 text-slate-200 text-left">
               Description
             </th>
           </tr>
@@ -27,10 +27,10 @@ const Notation = ({}: NotationProps) => {
         <tbody>
           {notations.map((notation, index) => (
             <tr key={index}>
-              <td className="border-b border-slate-700 p-4 pl-8 text-slate-400 break-keep">
+              <td className="border-b border-slate-700 p-2 pl-8 text-slate-200 break-keep">
                 {notation.notation}
               </td>
-              <td className="border-b border-slate-700 p-4 pl-8 text-slate-400 break-keep">
+              <td className="border-b border-slate-700 p-2 pl-8 text-slate-200 break-keep">
                 {notation.description}
               </td>
             </tr>
